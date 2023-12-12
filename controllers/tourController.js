19bff9b5b35312420d29fd91913ee2219e6598fe
AhmadAbukhader <130713263+AhmadAbukhader@users.noreply.github.com>
2023-12-12 20:57:06 +0200

    fourth commit

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,68 +1,87 @@
-const Tour = require ('./../models/tourModel')
+const Tour = require('./../models/tourModel')
 
 //const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)) this is for testing 
 
 
 exports.getAllTours = async (req, res) => {
     try {
-          //find will return an array of these document and then convert them to java script objects 
-    const tours = await Tour.find()
+        //build the query
+        const queryObj = {...req.query}
+        const excludedFields = ['page' , 'sort' , 'limit' , 'fields'];
+        excludedFields.forEach(el => delete queryObj[el])
 
-    res.status(200).json({
-        status: 'success',
-        result: tours.length,
-        data: {
-           tours
-        }
-    })
+        console.log(req.query , queryObj);
+
+        //find will return an array of these document and then convert them to java script objects 
+        const query = await Tour.find(queryObj)
+
+        // const query = await Tour.find()
+        //   .where('duration')
+        //   .equals(5)
+        //   .where('difficulty')
+        //   .equals('easy')
+
+        //execute the query
+
+        const tours = await query 
+
+        //send response 
+
+        res.status(200).json({
+            status: 'success',
+            result: tours.length,
+            data: {
+                tours
+            }
+        })
     } catch (err) {
-      res.status(404).json({
-        status : 'failed',
-        message:err 
-      })
+        res.status(404).json({
+            status: 'failed',
+            message: err
+        })
     }
 }
 exports.getTour = async (req, res) => {
-   try {
-    const tour = await Tour.findById(req.params.id)
-    //Tour.findOne({_id:req.params.id}) and it is _id because this how it look`s like in mongo db
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour
-        }
-    })
-   } catch (err) {
-    res.status(404).json({
-        status : 'failed',
-        message:err 
-      })
-   }
+    try {
+        const tour = await Tour.findById(req.params.id)
+        //Tour.findOne({_id:req.params.id}) and it is _id because this how it look`s like in mongo db
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour
+            }
+        })
+    } catch (err) {
+        res.status(404).json({
+            status: 'failed',
+            message: err
+        })
+    }
 }
 exports.createTour = async (req, res) => {
     try {
-    //the old method to create object 
-    // const newTour = new Tour ({})
-    // newTour.save()
-    const newTour = await Tour.create(req.body)
-    res.status(201).json({
-        status: "success",
-        data: {
-            tour: newTour
-        }
-    })
+        //the old method to create object 
+        // const newTour = new Tour ({})
+        // newTour.save()
+        const newTour = await Tour.create(req.body)
+        res.status(201).json({
+            status: "success",
+            data: {
+                tour: newTour
+            }
+        })
     } catch (error) {
         res.status(400).json({
-            status : "failed" , 
-            message : error
+            status: "failed",
+            message: error
         })
     }
 }
 
 exports.updateTour = async (req, res) => {
-    try{
-        const tour = await Tour.findByIdAndUpdate(req.params.id , req.body , {
-            new : true ,
+    try {
+        const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
             runValidators: true
         })
         res.status(200).json({
@@ -71,11 +90,11 @@ exports.updateTour = async (req, res) => {
                 tour
             }
         })
-    }catch(err){
+    } catch (err) {
         res.status(404).json({
-            status : 'failed',
-            message:err 
-          })
+            status: 'failed',
+            message: err
+        })
     }
 }
 exports.deleteTour = async (req, res) => {
@@ -87,8 +106,8 @@ exports.deleteTour = async (req, res) => {
         })
     } catch (err) {
         res.status(404).json({
-            status : 'failed',
-            message:err 
+            status: 'failed',
+            message: err
         })
     }
 }
